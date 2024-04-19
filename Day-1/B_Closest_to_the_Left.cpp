f01a8f6b0c55baa9d48cc52dd8fc493183ad5a75
MD MUSHFIQUR TALHA KHONDOKER <161893411+talha-khondoker@users.noreply.github.com>
2024-04-19 15:47:10 +0600

    Add files via upload

---
+++ Day-1/B_Closest_to_the_Left.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll n, k;
+    cin>>n>>k;
+    ll a[n];
+    for(int i=0; i<n; i++)
+    {
+        cin>>a[i];
+    }
+    while (k--)
+    {
+        ll key;
+        cin>>key;
+        ll l=0, r=n-1;
+        ll mid; 
+        ll idx=-1;
+        while (l<=r)
+        {
+            mid=(l+r)/2;
+            if(key>=a[mid])
+            {
+                idx=mid; l=mid+1;
+            }
+            else 
+            {
+                r=mid-1;
+            }
+        }
+        cout<<idx+1<<'\n';
+    }
+    
+    return 0;
+}
+// tc- logN
