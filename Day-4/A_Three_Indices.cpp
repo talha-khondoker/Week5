b96fabb817455acda3e2c79e380ab47ae37c22d6
MD MUSHFIQUR TALHA KHONDOKER <161893411+talha-khondoker@users.noreply.github.com>
2024-04-22 23:38:15 +0600

    Add files via upload

--- Day-4/A_Three_Indices.cpp
+++ Day-4/A_Three_Indices.cpp
@@ -7,9 +7,40 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     ll tc;
+    cin>>tc;
     while (tc--)
     {
-        /* code */
+        int n;
+        cin>>n;
+        int p[n];
+        for(int i=0; i<n; i++)
+        {
+            cin>>p[i];
+        }
+        bool flag=false;
+        int x, y,z;
+        for(int i=1; i<n-1; i++)
+        {
+            if(p[i-1] < p[i])
+            {
+                if(p[i] > p[i+1])
+                {
+                    x = i;
+                    y = i + 1;
+                    z = i + 2;
+                    flag=true;
+                    break;
+                }
+            }
+        }
+        if(flag) 
+        {
+            cout<<"YES"<<'\n'<<x<<" "<<y<<" "<<z<<'\n';
+        }
+        else
+        {
+            cout<<"NO"<<'\n';
+        }
     }
     
     return 0;
